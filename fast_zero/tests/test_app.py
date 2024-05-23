7894c295684e78e0313e45ede579ddfa4e1836dc
svapollo <sv.apollo11@gmail.com>
2024-05-23 15:10:45 -0300

    :test_tube: test: verbos http

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,13 +1,115 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_root_html_deve_retornar_ok_e_ola_mundo_html(client):
+    response_html = client.get('/html')
+
+    assert response_html.status_code == HTTPStatus.OK
+    assert '<html>' in response_html.text
+
+
+def test_create_user_deve_retonar_201(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_create_user_email_errado_deve_retornar_422(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'aliceexample.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+def test_read_users_deve_retornar_post_sucesso_anterior(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'alice', 'email': 'alice@example.com', 'id': 1}]
+    }
+
+
+def test_update_user_deve_retornar_200(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1
+    }
+
+
+def test_update_deve_retornar_404(client):
+    response = client.put(
+        '/users/10',
+        json={
+            'username': 'joao',
+            'email': 'joao@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user_deve_retornar_200(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_deve_retornar_404(client):
+    response = client.delete('/users/100')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_read_user_deve_retornar_200(client):
+    client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
