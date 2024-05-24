f691da59fc6da099f78e2ef90125c28f4962a796
svapollo <sv.apollo11@gmail.com>
2024-05-24 12:35:38 -0300

    :test_tube: test: usar banco de dados em memoria

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -45,33 +47,37 @@ def test_create_user_email_errado_deve_retornar_422(client):
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test_read_users_deve_retornar_post_sucesso_anterior(client):
+def test_read_users_deve_retornar_vazio_e_200(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [{'username': 'alice', 'email': 'alice@example.com', 'id': 1}]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user_deve_retornar_200(client):
+def test_read_user_deve_retornar_usuario_e_200(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user_deve_retornar_200(client, user):
     response = client.put(
         '/users/1',
         json={
             'username': 'bob',
             'email': 'bob@example.com',
-            'password': 'mynewpassword'
-        }
+            'password': 'mynewpassword',
+        },
     )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'username': 'bob',
         'email': 'bob@example.com',
-        'id': 1
+        'id': 1,
     }
 
 
-def test_update_deve_retornar_404(client):
+def test_update_deve_retornar_404(client, user):
     response = client.put(
         '/users/10',
         json={
@@ -84,32 +90,24 @@ def test_update_deve_retornar_404(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user_deve_retornar_200(client):
+def test_delete_user_deve_retornar_200(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_deve_retornar_404(client):
+def test_delete_deve_retornar_404(client, user):
     response = client.delete('/users/100')
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_read_user_deve_retornar_200(client):
-    client.post(
-        '/users/',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
+def test_read_user_deve_retornar_200(client, user):
     response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
+        'username': 'Test',
+        'email': 'test@test.com',
         'id': 1,
     }
